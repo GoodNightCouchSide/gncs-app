3510f0694fc4b452d94dcff2921bc96eb1d49eaa
Tim <tk.tim.flanke@gmail.com>
2022-12-25 15:17:44 +0100

    add main layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../features/Header'
+
+export interface IMainLayoutProps {}
+
+const MainLayout: React.FunctionComponent<IMainLayoutProps> = (props) => {
+  return (
+    <main style={{ border: 2, padding: 2, borderColor: 'black', borderStyle: 'solid' }}>
+      <Header />
+      <Outlet />
+    </main>
+  )
+}
+
+export default MainLayout
