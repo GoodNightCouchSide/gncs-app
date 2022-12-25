1126c0f319ba3312788b1bbb4f16b8176c7a54a9
Tim <tk.tim.flanke@gmail.com>
2022-12-25 15:20:43 +0100

    add user redux slice

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '../../rdx/store'
+
+interface UserState {
+  name?: string
+  isLogin: boolean
+}
+
+export interface UserLoginPayload {
+  name: string
+}
+
+const initialState: UserState = {
+  isLogin: false
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginUser: (state, action: PayloadAction<UserLoginPayload>) => {
+      return { ...state, name: action.payload.name }
+    }
+  }
+})
+
+export const { loginUser } = userSlice.actions
+
+export const selectUser = (state: RootState) => state.user
+
+export default userSlice.reducer
